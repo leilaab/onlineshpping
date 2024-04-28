053a5ce8bc60f79610cd9592dca02fbe6a581a4b
leilaab <sahel_70@yahoo.com>
2024-04-28 21:56:14 +0330

    react

---
+++ src/data/products.js
@@ -0,0 +1,40 @@
+  import mobile from "../assets/mobile.jpg"
+  import bag from "../assets/bag.jpg"
+  import camera from "../assets/camera.png"
+  import highheel from   "../assets/high-heel.jpg"
+  import  laptop from "../assets/laptop.jpg"
+  import  shoes from "../assets/shoes.jpg"
+  import  sunglass from "../assets/sunglass.webp"
+  
+  export const PRODUCTS = [
+  {
+    id: 1,
+    productName : " mobile",
+    price : 1000,
+    productImage : mobile
+  },
+  {
+    id: 2,
+    productName : " laptop",
+    price : 1500,
+    productImage : laptop
+  },
+  {
+    id: 3,
+    productName : "shoes",
+    price : 1000,
+    productImage :shoes
+  },
+  {
+    id: 4,
+    productName : "bag",
+    price : 700,
+    productImage :bag
+  },
+  {
+    id: 5,
+    productName : "camera",
+    price : 1500,
+    productImage :camera
+  }
+];
